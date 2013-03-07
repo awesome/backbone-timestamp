3c6f7a02aefdaa0d3f2682d5e2d8bc02ee9b49ca
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-03-07 16:45:58 +0600

    Added unit tests for Backbone.Timestamp

--- test/backbone_timestamp_test.js
+++ test/backbone_timestamp_test.js
@@ -1,2 +1,62 @@
 describe('Backbone.Timestamp', function() {
+  var expect = chai.expect;
+
+  var Book = Backbone.Model.extend({
+    urlRoot: '/api/books',
+    defaults: {
+      title: 'Red Book',
+      author: 'unknown'
+    }
+  });
+
+  Backbone.Timestamp(Book);
+
+  var Library = Backbone.Collection.extend({
+    model: Book,
+    url: 'api/library'
+  });
+
+  var currentDate = new Date();
+  sinon.stub(window, 'Date').returns(currentDate);
+  sinon.stub(Backbone, 'sync');
+
+  describe('on save model', function() {
+    it('sets createdAt and updatedAt automatically', function() {
+      var book      = new Book()
+        , updatedAt = '2013-03-07T10:05:45.260Z';
+
+      book.save({ title: 'War and Peace', updatedAt: updatedAt });
+
+      expect(book.get('title')).equal('War and Peace');
+      expect(book.get('author')).equal('unknown');
+
+      expect(book.get('createdAt')).equal(currentDate);
+      expect(book.get('updatedAt')).equal(updatedAt);
+    });
+
+    it('updates only not changed attributes', function() {
+      var createdAt = '2013-03-07T08:54:31.170Z'
+        , updatedAt = '2013-03-07T10:05:45.260Z'
+        , book      = new Book({ id: 1, content: 'Romeo & Juliet', createdAt: createdAt });
+
+      book.save();
+      expect(book.get('createdAt')).equal(createdAt);
+      expect(book.get('updatedAt')).equal(currentDate);
+
+      book.save({ updatedAt: updatedAt });
+      expect(book.get('createdAt')).equal(createdAt);
+      expect(book.get('updatedAt')).equal(updatedAt);
+    });
+  });
+
+  describe('on create model to collection', function() {
+    it('sets necessary attributes', function() {
+      var library   = new Library()
+        , createdAt = '2013-03-07T08:54:31.170Z'
+        , book      = library.create({ title: 'Moby dick', author: 'Herman Melville', createdAt: createdAt });
+
+      expect(book.get('createdAt')).equal(createdAt);
+      expect(book.get('updatedAt')).equal(currentDate);
+    });
+  });
 });
