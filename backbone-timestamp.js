aefa6011e5bf9abb43c89082f7aa50db7647002f
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-06-04 13:57:02 +0600

    Removed LICENCE file to use mit-license.org

--- backbone-timestamp.js
+++ backbone-timestamp.js
@@ -1,5 +1,5 @@
-// ...
-// https://github.com/Ask11/backbone-timestamp
+// Plugin for Backbone.Model to generate timestamps attributes
+// https://github.com/ask11/backbone-timestamp
 //
 // (c) 2013, Aleksey Kulikov
 // May be freely distributed according to MIT license.
